0982c0c7d337cfcb410e964d4e81d6463bc1718d
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-18 11:32:54 +0200

    docs: sign useful object properties from postman

--- assets/main.js
+++ assets/main.js
@@ -11,3 +11,5 @@ console.log('im alive');
 // Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSON Placeholder, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
 // Bonus
 // rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
+
+// si possono sfruttare title: (su card body) e url: (su card title)
